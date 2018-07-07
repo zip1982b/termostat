0ee2e2b7cebcc4d38801575ac211803b049fc72a
zip1982b <zip1982b@gmail.com>
2018-07-07 19:28:03 +0300

    test gpio

--- main/main.c
+++ main/main.c
@@ -18,6 +18,56 @@ Autor: zip1982b
 #include "driver/gpio.h"
 
 
+#define GPIO_RELAY1    14
+#define GPIO_RELAY2    12
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_RELAY1) | (1ULL<<GPIO_RELAY2))
+#define GPIO_ENC_CLK     33
+#define GPIO_ENC_DT		 25
+#define GPIO_ENC_SW		 26
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_ENC_CLK) | (1ULL<<GPIO_ENC_DT) | (1ULL<<GPIO_ENC_SW)) 
+#define ESP_INTR_FLAG_DEFAULT 0
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+
+static void ENC(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+		xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY);
+		if(io_num == GPIO_ENC_CLK)
+		{
+			//vTaskDelay(10 / portTICK_RATE_MS);
+			xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY);
+			if (io_num == GPIO_ENC_DT)
+			{
+				printf("clockwise rotation\n");
+			}
+				
+		}	
+		else if(io_num == GPIO_ENC_DT)
+		{
+			//vTaskDelay(10 / portTICK_RATE_MS);
+			xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY);
+			if (io_num == GPIO_ENC_CLK)
+			{
+				printf("counter clockwise rotation\n");
+			}
+				
+		}
+		else if(io_num == GPIO_ENC_SW)
+		{
+			printf("Button is pressed\n");
+		}
+    }
+}
+
+
+
 void app_main()
 {
 	printf("Project - Termostat\n");
@@ -34,10 +84,60 @@ void app_main()
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    vTaskDelay(10000 / portTICK_PERIOD_MS);
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+	
+	
+	/*** GPIO init ***/
+    gpio_config_t io_conf;
+	//Настройки GPIO для релейного ВЫХОДа
+    //disable interrupt - отключитли прерывания
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+	
+    //set as output mode - установка в режим Выход
+    io_conf.mode = GPIO_MODE_OUTPUT;
+	
+    //bit mask of the pins that you want to set,e.g.GPIO14/12
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+	
+    //disable pull-down mode - отключитли подтяжку к земле
+    io_conf.pull_down_en = 0;
+	
+    //disable pull-up mode - отключили подтяжку к питанию
+    io_conf.pull_up_en = 0;
+	
+    //configure GPIO with the given settings - конфигурирование портов с данными настройками (Выход)
+    gpio_config(&io_conf);
+	
+	//Настройки GPIO Энкодера на ВХОД
+	//interrupt of falling edge - прерывание по спадающему фронту и по возрастающему фронту
+    io_conf.intr_type = GPIO_INTR_ANYEDGE;
+    //bit mask of the pins, use GPIO33/25/26 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode    
+    io_conf.mode = GPIO_MODE_INPUT;
+    
+	//enable pull-up mode
+    //io_conf.pull_up_en = 1;
+	
+    gpio_config(&io_conf);
+	
+	//change gpio interrupt type for one pin
+    gpio_set_intr_type(GPIO_ENC_SW, GPIO_INTR_NEGEDGE); //Button pressed in gpio26
+	
+	
+	//create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(ENC, "ENC", 2048, NULL, 10, NULL);
+	
+	//install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_ENC_CLK, gpio_isr_handler, (void*) GPIO_ENC_CLK);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_ENC_DT, gpio_isr_handler, (void*) GPIO_ENC_DT);
+	//hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_ENC_SW, gpio_isr_handler, (void*) GPIO_ENC_SW);
+	
+	
 }
 
