8a01f4de252242648686059fc10c5aa6fd27e123
zip1982b <zip1982b@gmail.com>
2018-09-09 15:49:24 +0300

    change gpio ports for relay and display ssd1306 (i2c)

--- main/ssd1306_i2c.c
+++ main/ssd1306_i2c.c
@@ -27,8 +27,8 @@
 
 #define OLED_I2C_ADDRESS   0x3C
 
-#define I2C_MASTER_SCL_IO          19               /*!< gpio number for I2C master clock */
-#define I2C_MASTER_SDA_IO          18               /*!< gpio number for I2C master data  */
+#define I2C_MASTER_SCL_IO          26               /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO          25               /*!< gpio number for I2C master data  */
 #define I2C_MASTER_NUM             I2C_NUM_1        /*!< I2C port number for master dev */
 #define I2C_MASTER_TX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE  0                /*!< I2C master do not need buffer */
